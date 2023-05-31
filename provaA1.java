e595a13b92d5169147df7f424c696e2dd30945ae
João Victor Carvalho Dimas de Oliveira <130412515+Dimasjvco@users.noreply.github.com>
2023-05-31 09:50:30 -0300

    Update provaA1.java

--- provaA1.java
+++ provaA1.java
@@ -1,22 +1,11 @@
 import java.util.Scanner;
-public class provaA1 {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-
-    //Entrada
-    System.out.print("Digite o ano a ser verificado: ");
-    int ano = scanner.nextInt();
-
-    //Processamento e Saída
-    if (ano % 4 == 0 ) {
-      if (ano % 100 == 0 && ano % 400 != 0 )
-      System.out.printf("\nO ano " + ano + " não é bissexto");
-      else
-      System.out.printf("\nO ano " + ano + " é bissexto");
-    } else {
-      System.out.printf("\nO ano " + ano + " não é bissexto");
+public class anoBissexto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um ano: ");
+        int ano = scanner.nextInt();
+        String resultado = (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0) ? "bissexto" : "não bissexto";
+        System.out.println(ano + " é " + resultado + ".");
+        scanner.close();
     }
-        
-    scanner.close();
-  }
 }
